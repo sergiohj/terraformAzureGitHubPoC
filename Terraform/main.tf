4afd958ba7d6e74e069126a0bf6a6215fd590d3f
sergio <sergio.holgado@outlook.com>
2022-02-16 23:02:46 +0100

    Updated terraform file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -16,7 +16,6 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
  
-#Create Resource Group
 resource "azurerm_resource_group" "tamops" {
   name     = "tamops"
   location = "eastus2"
