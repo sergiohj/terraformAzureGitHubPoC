b583e2834306d2b9b42ed0de7f4cd0fcdfe347dc
sergio <sergio.holgado@outlook.com>
2022-02-16 23:00:39 +0100

    Added provider in main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,3 +6,10 @@ terraform {
     key                  = "tfstatedevops.tfstate"
   }
 }
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
