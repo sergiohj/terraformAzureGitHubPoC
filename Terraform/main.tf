a89762159bf84ff3ff96325ae1adfd646c7020d7
sergio <sergio.holgado@outlook.com>
2022-02-16 23:12:44 +0100

    updated main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,16 +6,13 @@ terraform {
     key                  = "tfstatedevops.tfstate"
   }
 }
-
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
 }
-
 data "azurerm_client_config" "current" {}
- 
 resource "azurerm_resource_group" "tamops" {
   name     = "tamops"
   location = "West Europe"
