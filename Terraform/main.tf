1c7f568d52781766e8494c1aef20e0b401cd4ad9
sergio <sergio.holgado@outlook.com>
2022-02-16 23:01:50 +0100

    Deploy resoruce group terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,3 +13,11 @@ provider "azurerm" {
   version = "~>2.0"
   features {}
 }
+
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "tamops"
+  location = "eastus2"
+}
