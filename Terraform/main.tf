90a8820dc5f7054f57f47cead824fa323233ce82
sergio <sergio.holgado@outlook.com>
2022-02-17 17:43:22 +0100

    Added SQL server

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,3 +14,11 @@ resource "azurerm_resource_group" "tamops" {
   name     = "tamops"
   location = "West Europe"
 }
+resource "azurerm_sql_server" "sqlserver" {
+  name                              = "terraformtestshj"
+  resource_group_name               = "tamops"
+  location                          = "West Europe"
+  version                           = "12.0"
+  administrator_login               = "admin"
+  administrator_login_password      = "Corp123!"
+}
