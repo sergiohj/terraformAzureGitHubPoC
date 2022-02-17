cfad912f65bbf6959398e8db2bdf6f4130dffc93
sergio <sergio.holgado@outlook.com>
2022-02-17 18:01:46 +0100

    New version MSSQK Server

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,30 +14,24 @@ resource "azurerm_resource_group" "tamops" {
   name     = "tamops"
   location = "West Europe"
 }
-resource "azurerm_sql_server" "example" {
+resource "azurerm_mssql_server" "sqlserver" {
   name                         = "mssqlservershj"
   resource_group_name          = "tamops"
   location                     = "West Europe"
   version                      = "12.0"
-  administrator_login          = "mradministrator"
-  administrator_login_password = "thisIsDog11"
-  tags = {
-    environment = "production"
-  }
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
-resource "azurerm_sql_database" "example" {
-  name                = "myexamplesqldatabaseshj"
-  resource_group_name = "tamops"
-  location            = "West US"
-  server_name         = "mssqlservershj"
+resource "azurerm_mssql_database" "test" {
+  name           = "acctest-db-d"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "BC_Gen5_2"
+  zone_redundant = true
   tags = {
-    environment = "production"
+    foo = "bar"
   }
-}
-resource "azurerm_sql_firewall_rule" "fw_connections_todb" {
-  name                = "allow-azure-services"
-  resource_group_name = "tamops"
-  server_name         = "mssqlservershj"
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
 }
