fd6e6077a4400a0879ba18a54c7ea2e8fcb76eed
sergio <sergio.holgado@outlook.com>
2022-02-17 17:19:48 +0100

    Added SQL server to terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,3 +17,31 @@ resource "azurerm_resource_group" "tamops" {
   name     = "tamops"
   location = "West Europe"
 }
+resource "azurerm_sql_server" "sqlserver" {
+  name                              = "terraformtestshj"
+  resource_group_name               = "tamops"
+  location                          = "West Europe"
+  version                           = "12.0"
+  administrator_login               = "admin"
+  administrator_login_password      = "Corp123!"
+}
+resource "azurerm_sql_database" "appdb" {
+  name                              = "test"
+  resource_group_name               = "tamops"
+  location                          = "West Europe"
+  server_name                       = "terraformtestshj"
+  edition                           = "Basic"
+  collation                         = "SQL_Latin1_General_CP1_CI_AS"
+  create_mode                       = "Default"
+  requested_service_objective_name  = "Basic"
+  tags = {
+    environment = "dev"
+  }
+}
+resource "azurerm_sql_firewall_rule" "fw_connections_todb" {
+  name                = "allow-azure-services"
+  resource_group_name = "tamops"
+  server_name         = "terraformtestshj"
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
