e81895f02c5d668bdcd9f246056ec00deb4e44be
sergio <sergio.holgado@outlook.com>
2022-02-17 17:47:52 +0100

    SQL Database

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -21,7 +21,15 @@ resource "azurerm_sql_server" "example" {
   version                      = "12.0"
   administrator_login          = "mradministrator"
   administrator_login_password = "thisIsDog11"
-
+  tags = {
+    environment = "production"
+  }
+}
+resource "azurerm_sql_database" "example" {
+  name                = "myexamplesqldatabase"
+  resource_group_name = "tamops"
+  location            = "West US"
+  server_name         = "mssqlserver"
   tags = {
     environment = "production"
   }
