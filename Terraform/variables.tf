950275094468f654b7ed69cd493ccf6232913657
sergio <sergio.holgado@outlook.com>
2022-02-23 09:12:52 +0100

    Update file vars

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -2,7 +2,7 @@ variable "region" {
   description = "Azure region to be used to deploy"
   type        = string
   default     = "West Europe"
-} 
+}
 variable "resourgroupname" {
   description = "Resource group name to be used to deploy resources"
   type        = string
